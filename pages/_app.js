d96a8706fa18fa90b883d43d6ce10803701ff395
Nolibert Eligan <krooohcho@gmail.com>
2024-02-02 19:45:53 +0800

    2nd comm

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css';
+import Transition from '../components/Transition'
+import Layout from '../components/Layout'
+import {useRouter} from 'next/router'
+import { AnimatePresence, motion } from 'framer-motion'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  return (
+    <Layout>
+      <AnimatePresence>
+        <div>
+          
+        </div>
+        <Component {...pageProps} />
+      </AnimatePresence>
+    </Layout>
+  )
 }
 
 export default MyApp;
